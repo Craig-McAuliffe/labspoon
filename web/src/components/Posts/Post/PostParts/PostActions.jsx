b0ad7a6093883376d69c179806af9e1001226c4c
Patrick Leask <patrickaaleask@gmail.com>
2020-09-17 12:59:50 +0100

    fix bookmark post actions when user is not signed in

--- web/src/components/Posts/Post/PostParts/PostActions.jsx
+++ web/src/components/Posts/Post/PostParts/PostActions.jsx
@@ -19,7 +19,7 @@ function BookmarkPostButton({post}) {
 
   // set the initial state of the bookmark
   useEffect(() => {
-    if (!featureFlags.has('disable-cloud-firestore')) {
+    if (user && !featureFlags.has('disable-cloud-firestore')) {
       db.collection(`users/${user.uid}/bookmarks`)
         .where('bookmarkedResourceType', '==', 'post')
         .where('bookmarkedResourceID', '==', post.id)
@@ -40,7 +40,7 @@ function BookmarkPostButton({post}) {
   // update the status of the bookmark
   useEffect(() => {
     if (!featureFlags.has('disable-cloud-firestore')) {
-      if (!firstRender) {
+      if (user && !firstRender) {
         const bookmarkCollection = db.collection(`users/${user.uid}/bookmarks`);
         if (bookmarked) {
           bookmarkCollection
