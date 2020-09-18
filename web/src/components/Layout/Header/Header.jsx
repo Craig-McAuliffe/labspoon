11cf17a63e487cbec121d0f83eaff6c2f3b754ba
Patrick Leask <patrickaaleask@gmail.com>
2020-09-18 13:36:33 +0100

    turn navbar into form

--- web/src/components/Layout/Header/Header.jsx
+++ web/src/components/Layout/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import AvatarDropDown from './AvatarDropDown';
 import HeaderLogo from '../../../assets/HeaderLogo';
 import {SearchIcon, SearchIconSmall} from '../../../assets/HeaderIcons';
 import './Header.css';
+import {useState} from 'react';
 
 const Header = () => {
   return (
@@ -13,22 +14,43 @@ const Header = () => {
           <HeaderLogo />
         </div>
       </Link>
-      <div className="header-search">
-        <div className="search-bar">
-          <div className="search-icon">
-            <SearchIcon />
-          </div>
-          <div className="search-icon-small">
-            <SearchIconSmall />
-          </div>
-          <input placeholder=" Search" type="text" />
-        </div>
-      </div>
+      <SearchBar />
       <div className="header-drop-down">
         <AvatarDropDown />
       </div>
     </div>
   );
 };
+
+function SearchBar() {
+  const [query, setQuery] = useState(undefined);
+  const history = useHistory();
+  function onSubmit(event) {
+    event.preventDefault();
+    history.push('search/' + query);
+  }
+  return (
+    <div className="header-search">
+      <div className="search-bar">
+        <form onSubmit={onSubmit}>
+          <button type="submit" value="Submit">
+            <div className="search-icon">
+              <SearchIcon />
+            </div>
+            <div className="search-icon-small">
+              <SearchIconSmall />
+            </div>
+          </button>
+          <input
+            placeholder="Search"
+            type="text"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Header;
