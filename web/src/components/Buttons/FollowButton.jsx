4de13e6586abf1f1936f15743d94fffcb10c399b
Craig McAuliffe <cmcauliffe@labspoon.com>
2020-09-25 14:17:53 +0100

    Add sign-up prompt on follow attempt

--- web/src/components/Buttons/FollowButton.jsx
+++ web/src/components/Buttons/FollowButton.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
-
+import React, {useState, useContext, useRef, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import {AuthContext} from '../../App';
 import './Buttons.css';
 
 const FollowButton = ({following, setFollowing}) => {
+  const [signUpPrompt, setSignUpPrompt] = useState(false);
+  const {user} = useContext(AuthContext);
+  const signUpPromptRef = useRef();
+
+  const followAction = () => {
+    user ? setFollowing(!following) : setSignUpPrompt(true);
+  };
+
+  useEffect(() => {
+    const handleDocumentClick = (e) => {
+      if (signUpPromptRef.current) {
+        if (
+          !signUpPromptRef.current.contains(e.target) &&
+          signUpPrompt === true
+        )
+          setSignUpPrompt(false);
+      }
+    };
+    document.addEventListener('mousedown', handleDocumentClick);
+  });
+
   return (
-    <button
-      className={following ? 'primary-button-clicked' : 'primary-button'}
-      onClick={() => setFollowing(!following)}
-    >
-      <div className="primary-button-text">
-        <h2>{following ? 'Unfollow' : 'Follow'}</h2>
-      </div>
-    </button>
+    <div className="follow-button-position">
+      <button
+        className={following ? 'primary-button-clicked' : 'primary-button'}
+        onClick={followAction}
+      >
+        <div className="primary-button-text">
+          <h2>{following ? 'Unfollow' : 'Follow'}</h2>
+        </div>
+      </button>
+      {signUpPrompt ? (
+        <div className="sign-up-prompt" ref={signUpPromptRef}>
+          <Link to="/login">Sign up to follow this.</Link>
+        </div>
+      ) : null}
+    </div>
   );
 };
+
 export default FollowButton;
