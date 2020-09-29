c3b0ac74383d62edea5a3540f3340cee24af9f46
Craig McAuliffe <cmcauliffe@labspoon.com>
2020-09-29 15:51:36 +0100

    Move bookmark logic to bookmark buton.

--- web/src/components/Buttons/BookmarkButton.jsx
+++ web/src/components/Buttons/BookmarkButton.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useContext, useEffect} from 'react';
+import {FeatureFlags, AuthContext} from '../../App';
+import {db} from '../../firebase';
 import {
   BookmarkIconUnselected,
   BookmarkIconSelected,
@@ -6,11 +8,15 @@ import {
 
 import './Buttons.css';
 
-const BookmarkButton = ({bookmarked, setBookmarked}) => {
-  const onClick = () => {
-    setBookmarked(!bookmarked);
-  };
-  return (
+function BookmarkButton({post}) {
+  const [bookmarked, setBookmarked] = useState(false);
+  const [bookmarkID, setBookmarkID] = useState(undefined);
+  // don't toggle when setting the current value
+  const [firstRender, setFirstRender] = useState(true);
+  const featureFlags = useContext(FeatureFlags);
+  const {user} = useContext(AuthContext);
+
+  const bookmarkButtonDisplay = () => (
     <div className="button-container">
       <button className="action-button" href="/" onClick={onClick}>
         {bookmarked ? <BookmarkIconSelected /> : <BookmarkIconUnselected />}
@@ -18,6 +24,66 @@ const BookmarkButton = ({bookmarked, setBookmarked}) => {
       </button>
     </div>
   );
-};
+
+  const onClick = () => {
+    setBookmarked(!bookmarked);
+  };
+
+  // set the initial state of the bookmark
+  useEffect(() => {
+    if (user && !featureFlags.has('disable-cloud-firestore')) {
+      db.collection(`users/${user.uid}/bookmarks`)
+        .where('bookmarkedResourceType', '==', 'post')
+        .where('bookmarkedResourceID', '==', post.id)
+        .get()
+        .then((qs) => {
+          if (!qs.empty) {
+            qs.forEach((bookmark) => {
+              setBookmarkID(bookmark.id);
+            });
+            setBookmarked(true);
+          }
+          setFirstRender(false);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
+  // update the status of the bookmark
+  useEffect(() => {
+    if (!featureFlags.has('disable-cloud-firestore')) {
+      if (user && !firstRender) {
+        const bookmarkCollection = db.collection(`users/${user.uid}/bookmarks`);
+        if (bookmarked) {
+          bookmarkCollection
+            .add({
+              resourceType: 'bookmark',
+              bookmarkedResourceType: 'post',
+              bookmarkedResourceID: post.id,
+              bookmarkedResourceData: post,
+            })
+            .then((docRef) => {
+              setBookmarked(true);
+              setBookmarkID(docRef.id);
+            })
+            .catch((err) => console.log(err));
+        } else {
+          bookmarkCollection
+            .doc(bookmarkID)
+            .delete()
+            .then(() => setBookmarkID(undefined))
+            .catch((err) => console.log(err));
+        }
+      }
+    }
+  }, [bookmarked]);
+
+  if (!post) {
+    setBookmarked(false);
+    return bookmarkButtonDisplay();
+  }
+
+  return bookmarkButtonDisplay();
+}
 
 export default BookmarkButton;
