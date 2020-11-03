c03f7e8d4dd94a9b27a714ccb74917c9bb4f0930
Craig McAuliffe <cmcauliffe@labspoon.com>
2020-11-03 13:59:09 +0000

    Put NotFoundPage in center of screen

--- web/src/pages/NotFoundPage/NotFoundPage.jsx
+++ web/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function NotFoundPage() {
-  return <h2>Not found</h2>;
+  return (
+    <div className="content-layout">
+      <div className="page-content-container">
+        <h2>Not found</h2>
+      </div>
+    </div>
+  );
 }
