8b20f572822f1b59b1587cf364c63fe5b80a8b48
patrick <patrick@labspoon.com>
2020-11-19 16:36:37 +0000

    add back useparams to publication page

--- web/src/pages/ResourcePages/PublicationPage/PublicationPage.jsx
+++ web/src/pages/ResourcePages/PublicationPage/PublicationPage.jsx
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {FeatureFlags} from '../../../App';
 import {db} from '../../../firebase';
+import {useParams} from 'react-router-dom';
 
 import {
   dbPublicationToJSPublication,
