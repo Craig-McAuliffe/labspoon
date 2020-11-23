0bef73517a3bdb9681983c8af1d9e5e854c30951
Craig McAuliffe <cmcauliffe@labspoon.com>
2020-11-23 08:06:09 +0000

    Improve MAG router front end

--- web/src/components/MAGRouter.jsx
+++ web/src/components/MAGRouter.jsx
@@ -31,16 +31,19 @@ export default function MAGRouterDisplay({query, formatRedirectPath}) {
 }
 
 const NotFound = () => (
-  <>
-    <h1>Not Found</h1>
-    <p>
-      This probably just means we haven&rsquo;t had a chance to process this
-      resource yet. That means you&rsquo;re literally faster than the speed of
-      light travelling through our data centres. Whilst you&rsquo;ve been
-      distracted by this message, we&rsquo;ve probably had a chance to catch up
-      with you, so just hit reload!
-    </p>
-  </>
+  <div className="content-layout">
+    <div className="details-container">
+      <h1 className="not-found-title">Not Found</h1>
+      <h3 className="not-found-title">You&rsquo;re going too fast for us!</h3>
+      <p>
+        You&rsquo;re probably seeing this page because we haven&rsquo;t had a
+        chance to process this resource yet. That means you&rsquo;re literally
+        faster than the speed of light travelling through our data centres.
+        Whilst you&rsquo;ve been distracted by this message, we&rsquo;ve
+        probably had a chance to catch up with you, so just refresh the page!
+      </p>
+    </div>
+  </div>
 );
 
 const Error = () => <h1>An error occurred</h1>;
