bb39b8af5219381c452a6616086d955604a97c29
patrick <patrick@labspoon.com>
2020-12-14 07:41:46 +0000

    re-enable dedicated post page

--- web/src/helpers/posts.js
+++ web/src/helpers/posts.js
@@ -20,23 +20,22 @@ export function getPaginatedPostsFromCollectionRef(
 // Optional fields are stored in the content of the post, however the post component expects them
 // in an optional tags field.
 export function translateOptionalFields(posts) {
-  return posts.map((result) => {
-    Object.entries(result.content).forEach((value) => {
-      let [type, content] = value;
-      if (type === 'text') return;
-      if (type === 'researchers') type = 'researcher';
-      if (
-        !result.hasOwnProperty('optionaltags') ||
-        result.optionaltags.length === 0
-      )
-        result.optionaltags = [];
-      result.optionaltags.push({
-        type: type,
-        content: content,
-      });
+  return posts.map((post) => translateOptionalField(post));
+}
+
+export function translateOptionalField(post) {
+  Object.entries(post.content).forEach((value) => {
+    let [type, content] = value;
+    if (type === 'text') return;
+    if (type === 'researchers') type = 'researcher';
+    if (!post.hasOwnProperty('optionaltags') || post.optionaltags.length === 0)
+      post.optionaltags = [];
+    post.optionaltags.push({
+      type: type,
+      content: content,
     });
-    return result;
   });
+  return post;
 }
 
 export function postsQSToJSPosts(qs) {
