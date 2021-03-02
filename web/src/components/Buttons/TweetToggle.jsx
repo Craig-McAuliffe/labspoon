1fb645f8cd085041dca31e0f6041a02180a0d0c8
Craig McAuliffe <cmcauliffe@labspoon.com>
2021-03-02 14:59:48 +0000

    add info for TweetToggle onHover

--- web/src/components/Buttons/TweetToggle.jsx
+++ web/src/components/Buttons/TweetToggle.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import twitterIconBlue from '../../assets/Twitter-logo-blue.png';
 import './TweetToggle.css';
 
 export default function TweetToggle({checked, setChecked}) {
+  const [isInfoDisplayed, setIsInfoDisplayed] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+
+  useEffect(() => {
+    if (!isHovering) {
+      if (isInfoDisplayed) setIsInfoDisplayed(false);
+      return;
+    }
+    const infoAfterTimeout = setTimeout(() => setIsInfoDisplayed(true), 800);
+    return () => clearTimeout(infoAfterTimeout);
+  }, [isHovering]);
+
   return (
-    <div className="tweet-toggle-container">
+    <div
+      className="tweet-toggle-container"
+      onMouseOver={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
+      <div
+        className={`tweet-toggle-info${
+          isInfoDisplayed ? '-visible' : '-hidden'
+        }`}
+      >
+        Redirects to your Twitter with the post pre-populated. Does not auto
+        tweet.
+      </div>
       <img
         src={twitterIconBlue}
         alt="Twitter logo"
