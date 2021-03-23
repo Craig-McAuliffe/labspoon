8b814b85efdb41b9f48b92e4696eecf1c680eb8e
Craig McAuliffe <cmcauliffe@labspoon.com>
2021-03-23 09:59:30 +0000

    use list items for articles

--- functions/src/researchFocuses.ts
+++ functions/src/researchFocuses.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import {admin} from './config';
-import {GroupRef} from './groups';
+import {GroupRef, groupRefToGroupSignature, GroupSignature} from './groups';
+import {Technique} from './techniques';
 import {TaggedTopic} from './topics';
 import {UserRef} from './users';
 
@@ -9,25 +10,26 @@ const db = admin.firestore();
 export const addResearchFocusToAuthor = functions.firestore
   .document(`researchFocuses/{researchFocusID}`)
   .onCreate((researchFocusDS) => {
-    const researchFocus = researchFocusDS.data();
+    const researchFocus = researchFocusDS.data() as ResearchFocus;
     const authorID = researchFocus.author.id;
     return db
       .doc(`users/${authorID}/researchFocuses/${researchFocusDS.id}`)
-      .set(researchFocus);
+      .set(articleToArticleListItem(researchFocus));
   });
 
 export const addResearchFocusToTopics = functions.firestore
   .document(`researchFocuses/{researchFocusID}`)
   .onCreate(async (researchFocusDS, context) => {
-    const researchFocus = researchFocusDS.data();
+    const researchFocus = researchFocusDS.data() as ResearchFocus;
     const researchFocusID = context.params.researchFocusID;
     const researchFocusTopics = researchFocus.topics;
+    if (!researchFocusTopics || researchFocusTopics.length === 0) return;
     const topicsPromises = researchFocusTopics.map(
       (taggedTopic: TaggedTopic) => {
         if (!taggedTopic.id) return;
         return db
           .doc(`topics/${taggedTopic.id}/researchFocuses/${researchFocusID}`)
-          .set(researchFocus)
+          .set(articleToArticleListItem(researchFocus))
           .catch((err) =>
             console.error(
               'unable to add research focus with id ' +
@@ -45,42 +47,61 @@ export const addResearchFocusToTopics = functions.firestore
 export const updateResearchFocusOnGroup = functions.firestore
   .document(`researchFocuses/{researchFocusID}`)
   .onUpdate((researchFocusDS) => {
-    const researchFocus = researchFocusDS.after.data();
-    const oldResearchFocusData = researchFocusDS.before.data();
+    const researchFocus = researchFocusDS.after.data() as ResearchFocus;
+    const oldResearchFocusData = researchFocusDS.before.data() as ResearchFocus;
     // if user edited article, this will already be updated
     if (
       JSON.stringify(researchFocus.body) !==
       JSON.stringify(oldResearchFocusData.body)
     )
       return;
+    if (
+      JSON.stringify(articleToArticleListItem(researchFocus)) ===
+      JSON.stringify(articleToArticleListItem(oldResearchFocusData))
+    )
+      return;
     const groupID = researchFocus.group.id;
     return db
       .doc(`groups/${groupID}/researchFocuses/${researchFocusDS.after.id}`)
-      .set(researchFocus);
+      .set(articleToArticleListItem(researchFocus));
   });
 
 export const updateResearchFocusOnAuthor = functions.firestore
   .document(`researchFocuses/{researchFocusID}`)
   .onUpdate((researchFocusDS) => {
-    const researchFocus = researchFocusDS.after.data();
+    const researchFocus = researchFocusDS.after.data() as ResearchFocus;
+    const oldResearchFocusData = researchFocusDS.before.data() as ResearchFocus;
+    if (
+      JSON.stringify(articleToArticleListItem(researchFocus)) ===
+      JSON.stringify(articleToArticleListItem(oldResearchFocusData))
+    )
+      return;
     const authorID = researchFocus.author.id;
+
     return db
       .doc(`users/${authorID}/researchFocuses/${researchFocusDS.after.id}`)
-      .set(researchFocus);
+      .set(articleToArticleListItem(researchFocus));
   });
 
 export const updateResearchFocusOnTopics = functions.firestore
   .document(`researchFocuses/{researchFocusID}`)
   .onUpdate(async (researchFocusDS, context) => {
-    const researchFocus = researchFocusDS.after.data();
+    const oldResearchFocusData = researchFocusDS.before.data() as ResearchFocus;
+    const researchFocus = researchFocusDS.after.data() as ResearchFocus;
     const researchFocusID = context.params.researchFocusID;
     const researchFocusTopics = researchFocus.topics;
+    if (
+      JSON.stringify(articleToArticleListItem(researchFocus)) ===
+      JSON.stringify(articleToArticleListItem(oldResearchFocusData))
+    )
+      return;
+    if (!researchFocusTopics || researchFocusTopics.length === 0) return;
     const topicsPromises = researchFocusTopics.map(
       (taggedTopic: TaggedTopic) => {
         if (!taggedTopic.id) return;
         return db
           .doc(`topics/${taggedTopic.id}/researchFocuses/${researchFocusID}`)
-          .set(researchFocus)
+          .set(articleToArticleListItem(researchFocus))
           .catch((err) =>
             console.error(
               'unable to update researchFocus with id ' +
@@ -95,6 +116,25 @@ export const updateResearchFocusOnTopics = functions.firestore
     return await Promise.all(topicsPromises);
   });
 
+export function articleToArticleListItem(
+  article: ResearchFocus | Technique
+): ResearchFocusListItem {
+  const researchFocusListItem: ResearchFocusListItem = {
+    title: article.title,
+    author: article.author,
+    timestamp: article.timestamp,
+    group: groupRefToGroupSignature(article.group, article.group.id),
+    body: article.body,
+  };
+  if (article.topics) researchFocusListItem.topics = article.topics;
+  if (article.customTopics)
+    researchFocusListItem.customTopics = article.customTopics;
+  if (article.photoURLs) researchFocusListItem.photoURLs = article.photoURLs;
+  if (article.filterTopicIDs)
+    researchFocusListItem.filterTopicIDs = article.filterTopicIDs;
+  return researchFocusListItem;
+}
+
 export interface ResearchFocus {
   title: string;
   author: UserRef;
@@ -103,6 +143,19 @@ export interface ResearchFocus {
   timestamp: Date;
   photoURLs?: string[];
   group: GroupRef;
+  filterTopicIDs?: string[];
+  body: ArticleBodyChild[];
+  id?: string;
+}
+
+export interface ResearchFocusListItem {
+  title: string;
+  author: UserRef;
+  topics?: TaggedTopic[];
+  customTopics?: string[];
+  timestamp: Date;
+  photoURLs?: string[];
+  group: GroupSignature;
   filterTopicIDs?: string[];
   body: ArticleBodyChild[];
   id?: string;
--- functions/src/techniques.ts
+++ functions/src/techniques.ts
@@ -3,31 +3,32 @@ import {admin} from './config';
 import {GroupRef} from './groups';
 import {TaggedTopic} from './topics';
 import {UserRef} from './users';
-import {ArticleBodyChild} from './researchFocuses';
+import {ArticleBodyChild, articleToArticleListItem} from './researchFocuses';
 
 const db = admin.firestore();
 
 export const addTechniqueToAuthor = functions.firestore
   .document(`techniques/{techniqueID}`)
   .onCreate((techniqueDS) => {
-    const technique = techniqueDS.data();
+    const technique = techniqueDS.data() as Technique;
     const authorID = technique.author.id;
     return db
       .doc(`users/${authorID}/techniques/${techniqueDS.id}`)
-      .set(technique);
+      .set(articleToArticleListItem(technique));
   });
 
 export const addTechniqueToTopics = functions.firestore
   .document(`techniques/{techniqueID}`)
   .onCreate(async (techniqueDS, context) => {
-    const technique = techniqueDS.data();
+    const technique = techniqueDS.data() as Technique;
     const techniqueID = context.params.techniqueID;
     const techniqueTopics = technique.topics;
+    if (!techniqueTopics || techniqueTopics.length === 0) return;
     const topicsPromises = techniqueTopics.map((taggedTopic: TaggedTopic) => {
       if (!taggedTopic.id) return;
       return db
         .doc(`topics/${taggedTopic.id}/techniques/${techniqueID}`)
-        .set(technique)
+        .set(articleToArticleListItem(technique))
         .catch((err) =>
           console.error(
             'unable to add technique with id ' +
@@ -44,40 +45,58 @@ export const addTechniqueToTopics = functions.firestore
 export const updateTechniqueOnGroup = functions.firestore
   .document(`techniques/{techniqueID}`)
   .onUpdate((techniqueDS) => {
-    const technique = techniqueDS.after.data();
-    const oldTechniqueData = techniqueDS.before.data();
+    const technique = techniqueDS.after.data() as Technique;
+    const oldTechniqueData = techniqueDS.before.data() as Technique;
     // if user edited article, this will already be updated
     if (
       JSON.stringify(technique.body) !== JSON.stringify(oldTechniqueData.body)
     )
       return;
+    if (
+      JSON.stringify(articleToArticleListItem(technique)) ===
+      JSON.stringify(articleToArticleListItem(oldTechniqueData))
+    )
+      return;
     const groupID = technique.group.id;
     return db
       .doc(`groups/${groupID}/techniques/${techniqueDS.after.id}`)
-      .set(technique);
+      .set(articleToArticleListItem(technique));
   });
 
 export const updateTechniqueOnAuthor = functions.firestore
   .document(`techniques/{techniqueID}`)
   .onUpdate((techniqueDS) => {
-    const technique = techniqueDS.after.data();
+    const technique = techniqueDS.after.data() as Technique;
+    const oldTechniqueData = techniqueDS.before.data() as Technique;
+    if (
+      JSON.stringify(articleToArticleListItem(technique)) ===
+      JSON.stringify(articleToArticleListItem(oldTechniqueData))
+    )
+      return;
     const authorID = technique.author.id;
     return db
       .doc(`users/${authorID}/techniques/${techniqueDS.after.id}`)
-      .set(technique);
+      .set(articleToArticleListItem(technique));
   });
 
 export const updateTechniqueOnTopics = functions.firestore
   .document(`techniques/{techniqueID}`)
   .onUpdate(async (techniqueDS, context) => {
-    const technique = techniqueDS.after.data();
+    const technique = techniqueDS.after.data() as Technique;
+    const oldTechniqueData = techniqueDS.before.data() as Technique;
     const techniqueID = context.params.techniqueID;
+    if (
+      JSON.stringify(articleToArticleListItem(technique)) ===
+      JSON.stringify(articleToArticleListItem(oldTechniqueData))
+    )
+      return;
     const techniqueTopics = technique.topics;
+    if (!techniqueTopics || techniqueTopics.length === 0) return;
     const topicsPromises = techniqueTopics.map((taggedTopic: TaggedTopic) => {
       if (!taggedTopic.id) return;
       return db
         .doc(`topics/${taggedTopic.id}/techniques/${techniqueID}`)
-        .set(technique)
+        .set(articleToArticleListItem(articleToArticleListItem(technique)))
         .catch((err) =>
           console.error(
             'unable to update technique with id ' +
